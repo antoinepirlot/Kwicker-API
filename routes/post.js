8849ee2141c82f6dbd5399fe5004d6f00e5053b7
antoine <pirlotantoine2@gmail.com>
2021-12-07 21:28:26 +0100

    Add : get posts for a specific user

--- routes/post.js
+++ routes/post.js
@@ -14,6 +14,17 @@ router.get("/", async (req, res) => {
     } catch (e){
         console.log(e.stack);
     }
-})
+});
+
+router.get("/:email", async (req, res) => {
+    console.log("GET/ : Posts from a user");
+    try{
+        const query = "SELECT p.id_post, p.id_user, p.image, p.message, p.parent_post, p.is_removed FROM kwicker.posts p, kwicker.users u WHERE p.id_user = u.id_user AND email = $1";
+        const {rows} = await db.query(query, [req.params.email]);
+        res.send(rows);
+    } catch (e){
+        console.log(e.stack);
+    }
+});
 
 module.exports = router;
