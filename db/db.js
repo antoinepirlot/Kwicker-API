d4e191141ee60a57756a81286d62d1f860b7deba
antoine <pirlotantoine2@gmail.com>
2021-12-07 20:53:35 +0100

    Update : use pg docuentation code

--- db/db.js
+++ db/db.js
@@ -1,52 +1,9 @@
+//From pg domcumentation
 const {Pool} = require("pg");
+const dbConfig = require("../config/db_config");
 
-const pool = new Pool();
-
-//pool.on from pg documentation
-pool.on('error', (err, client) => {
-    console.log('Unexpected error on idle client', err)
-    process.exit(1)
-});
-
-
-async function getUser(email) {
-    const query = `
-        SELECT *
-        FROM kwicker.get_all_users
-        WHERE "email" = $1::VARCHAR(100)
-    `;
-    try {
-        const client = await pool.connect();
-        try {
-            const client = await pool.connect();
-            const res = await client.query(query, [email]);
-            return res.rows;
-        } finally {
-            client.release();
-        }
-    } catch (e) {
-        console.log(e.stack);
-    }
-}
-
-async function getAllPosts() {
-
-    try {
-        const client = await pool.connect();
-        try {
-            const res = await client.query("SELECT * FROM kwicker.get_all_posts");
-            return res.rows;
-        } finally {
-            client.release();
-        }
-    } catch (e) {
-        console.log(e.stack);
-    }
-}
+const pool = new Pool(dbConfig);
 
 module.exports = {
-    getUser,
-    getAllPosts
+    query: (text, params) => pool.query(text, params)
 }
-
-//module.exports = pool;
