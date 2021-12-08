304ea75009c80017bf6499e9c2adb8a055ddfea5
antoine <pirlotantoine2@gmail.com>
2021-12-08 15:29:40 +0100

    Refactor: query are now in a query variable for better lisibility

--- routes/post.js
+++ routes/post.js
@@ -8,8 +8,10 @@ const router = express.Router();
  */
 router.get("/", async (req, res) => {
     console.log("GET/ : Posts");
+    const query = `SELECT *
+                   FROM kwicker.posts`;
     try {
-        const {rows} = await db.query("SELECT * FROM kwicker.posts");
+        const {rows} = await db.query(query);
         res.send(rows);
     } catch (e){
         console.log(e.stack);
@@ -18,8 +20,17 @@ router.get("/", async (req, res) => {
 
 router.get("/:email", async (req, res) => {
     console.log("GET/ : Posts from a user");
+    const query = `SELECT p.id_post,
+                          p.id_user,
+                          p.image,
+                          p.message,
+                          p.parent_post,
+                          p.is_removed
+                   FROM kwicker.posts p,
+                        kwicker.users u
+                   WHERE p.id_user = u.id_user
+                     AND u.email = $1`;
     try{
-        const query = "SELECT p.id_post, p.id_user, p.image, p.message, p.parent_post, p.is_removed FROM kwicker.posts p, kwicker.users u WHERE p.id_user = u.id_user AND email = $1";
         const {rows} = await db.query(query, [req.params.email]);
         res.send(rows);
     } catch (e){
