fef452b5970b3a5d1dce79f8b0f22ea7c1c64078
antoine <pirlotantoine2@gmail.com>
2021-12-09 15:01:24 +0100

    Refactor comments

--- routes/post.js
+++ routes/post.js
@@ -1,53 +1,40 @@
 const express = require("express");
 const db = require("../db/db");
+const {Posts} = require("../models/posts");
 
 const router = express.Router();
+const postsModel = new Posts();
 
 /**
  * GET all posts
  */
 router.get("/", async (req, res) => {
-    console.log("GET/ : Posts");
-    const query = `SELECT *
-                   FROM kwicker.posts`;
-    try {
-        const {rows} = await db.query(query);
-        res.send(rows);
-    } catch (e){
-        console.log(e.stack);
-    }
+    res.json(await postsModel.getAllPosts());
 });
 
-router.get("/:email", async (req, res) => {
+/**
+ * GET all posts from a user identified by its id
+ */
+router.get("/:id_user", async (req, res) => {
     console.log("GET/ : Posts from a user");
-    const query = `SELECT p.id_post,
-                          p.id_user,
-                          p.image,
-                          p.message,
-                          p.parent_post,
-                          p.is_removed
-                   FROM kwicker.posts p,
-                        kwicker.users u
-                   WHERE p.id_user = u.id_user
-                     AND u.email = $1`;
-    try{
-        const {rows} = await db.query(query, [req.params.email]);
-        res.send(rows);
+    try {
+        return res.json(await postsModel.getUserPosts(req.params.id_user));
     } catch (e){
-        console.log(e.stack);
-        res.sendStatus(502);
+        return res.sendStatus(502);
     }
 });
 
+/**
+ * POST add a new post to the db
+ */
 router.post("/", async (req, res) => {
     const body = req.body;
     let query = "INSERT INTO kwicker.posts (id_user, image, message, parent_post) VALUES ($1, $2, $3, $4)";
     try{
         await db.query(query, [body.id_user, body.image, body.message, body.parent_post]);
-        res.sendStatus(200);
     } catch (e){
         console.log(e.stack);
-        res.sendStatus(502);
+        return res.sendStatus(502);
     }
 });
 
