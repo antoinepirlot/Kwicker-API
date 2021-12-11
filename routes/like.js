175a354f58c774efb8d664ae30e7e67bef5a9cd4
antoine <pirlotantoine2@gmail.com>
2021-12-11 14:00:54 +0100

    Add the possibility to get all user's likes

--- routes/like.js
+++ routes/like.js
@@ -14,4 +14,18 @@ router.get("/", async (req, res) => {
     }
 });
 
+/**
+ * GET all user's likes
+ */
+router.get("/:id_user", async (req, res) => {
+    try{
+        const likes = await likesModel.getUserLikes(req.params.id_user);
+        if(likes.length === 0)
+            return res.sendStatus(404);
+        return res.json(likes);
+    } catch (e){
+        return res.sendStatus(502);
+    }
+});
+
 module.exports = router;
