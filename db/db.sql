f19a407ba7b27cc6f6d105cc818fa2a89cae5e49
antoine <pirlotantoine2@gmail.com>
2021-12-11 22:09:09 +0100

    Update db script with trigger while adding a new post

--- db/db.sql
+++ db/db.sql
@@ -1,33 +1,33 @@
 DROP SCHEMA IF EXISTS kwicker CASCADE;
 
 CREATE SCHEMA kwicker;
-SET TIMEZONE='Europe/Brussels';
+SET TIMEZONE = 'Europe/Brussels';
 
 CREATE TABLE kwicker.users
 (
-    id_user   SERIAL PRIMARY KEY,
-    forename  VARCHAR(50)  NOT NULL CHECK (forename <> ''),
-    lastname  VARCHAR(50)  NOT NULL CHECK (lastname <> ''),
-    email     VARCHAR(100) NOT NULL CHECK (email <> '') UNIQUE,
-    username  VARCHAR(100) NOT NULL CHECK (username <> '') UNIQUE,
-    image     VARCHAR(100) CHECK (image <> ''),
-    password  VARCHAR(100) NOT NULL CHECK (password <> ''),
-    is_active BOOLEAN      NOT NULL DEFAULT TRUE,
-    is_admin  BOOLEAN      NOT NULL DEFAULT FALSE,
-    biography VARCHAR(500) NULL,
-    date_creation DATE NOT NULL DEFAULT NOW()
+    id_user       SERIAL PRIMARY KEY,
+    forename      VARCHAR(50)  NOT NULL CHECK (forename <> ''),
+    lastname      VARCHAR(50)  NOT NULL CHECK (lastname <> ''),
+    email         VARCHAR(100) NOT NULL CHECK (email <> '') UNIQUE,
+    username      VARCHAR(100) NOT NULL CHECK (username <> '') UNIQUE,
+    image         VARCHAR(100) CHECK (image <> ''),
+    password      VARCHAR(100) NOT NULL CHECK (password <> ''),
+    is_active     BOOLEAN      NOT NULL DEFAULT TRUE,
+    is_admin      BOOLEAN      NOT NULL DEFAULT FALSE,
+    biography     VARCHAR(500) NULL,
+    date_creation DATE         NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE kwicker.posts
 (
-    id_post     SERIAL PRIMARY KEY,
-    id_user     INTEGER      NOT NULL,
-    image       VARCHAR(100) CHECK (image <> ''),
-    message     VARCHAR(300) NOT NULL CHECK (message <> ''),
-    parent_post INTEGER,
-    is_removed  BOOLEAN NOT NULL DEFAULT FALSE,
-    date_creation DATE NOT NULL DEFAULT NOW(),
-    number_of_likes INT NOT NULL DEFAULT 0 CHECK (number_of_likes >= 0),
+    id_post         SERIAL PRIMARY KEY,
+    id_user         INTEGER      NOT NULL,
+    image           VARCHAR(100) CHECK (image <> ''),
+    message         VARCHAR(300) NOT NULL CHECK (message <> ''),
+    parent_post     INTEGER,
+    is_removed      BOOLEAN      NOT NULL DEFAULT FALSE,
+    date_creation   DATE         NOT NULL DEFAULT NOW(),
+    number_of_likes INT          NOT NULL DEFAULT 0 CHECK (number_of_likes >= 0),
     FOREIGN KEY (id_user) REFERENCES kwicker.users (id_user),
     FOREIGN KEY (parent_post) REFERENCES kwicker.posts (id_post)
 );
@@ -56,17 +56,54 @@ CREATE TABLE kwicker.reports
     FOREIGN KEY (id_post) REFERENCES kwicker.posts (id_post)
 
 );
-TRIGGER LIKES
-INSERT INTO kwicker.users (forename, lastname, email, username, password) VALUES ('Antoine', 'Pirlot', 'antoine.pirlot@vinci.be', 'lepirelot' ,'mdp');
-INSERT INTO kwicker.users (forename, lastname, email, username, password) VALUES ('Denis', 'Victor', 'victor.denis@vinci.be', 'vivi','mdp');
-INSERT INTO kwicker.users (forename, lastname, email, username, password) VALUES ('Soulaymane', 'Gharroudi', 'soulaymane.gharroudi@vinci.be', 'souli','mdp');
-INSERT INTO kwicker.users (forename, lastname, email, username, password) VALUES ('François', 'Bardijn', 'francois.bardijn@vinci.be', 'françois', 'mdp');
 
-INSERT INTO kwicker.posts (id_user, message) VALUES (1, 'Hello World!');
-INSERT INTO kwicker.posts (id_user, message) VALUES (1, 'Bye World!');
-INSERT INTO kwicker.posts (id_user, message) VALUES (2, 'Je m''appelle Victor et j''aime les présentations orale en anglais');
-INSERT INTO kwicker.posts (id_user, message) VALUES (2, 'J''aime ma moto');
-INSERT INTO kwicker.posts (id_user, message) VALUES (3, 'Je lis des livres dans le métro et je rate l''arrêt :p');
-INSERT INTO kwicker.posts (id_user, message) VALUES (3, 'Ça bosse dure ici.');
-INSERT INTO kwicker.posts (id_user, message) VALUES (4, 'JS le sang');
-INSERT INTO kwicker.posts (id_user, message) VALUES (4, 'Je m''appelle François');
+CREATE OR REPLACE FUNCTION kwicker.add_like() RETURNS TRIGGER AS
+$$
+    BEGIN
+        UPDATE kwicker.posts
+        SET number_of_likes = number_of_likes + 1
+        WHERE id_post = NEW.id_post;
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_add_like
+    AFTER INSERT
+    ON kwicker.likes
+    FOR EACH ROW
+EXECUTE PROCEDURE kwicker.add_like();
+
+INSERT INTO kwicker.users (forename, lastname, email, username, password)
+VALUES ('Antoine', 'Pirlot', 'antoine.pirlot@vinci.be', 'lepirelot', 'mdp');
+INSERT INTO kwicker.users (forename, lastname, email, username, password)
+VALUES ('Denis', 'Victor', 'victor.denis@vinci.be', 'vivi', 'mdp');
+INSERT INTO kwicker.users (forename, lastname, email, username, password)
+VALUES ('Soulaymane', 'Gharroudi', 'soulaymane.gharroudi@vinci.be', 'souli', 'mdp');
+INSERT INTO kwicker.users (forename, lastname, email, username, password)
+VALUES ('François', 'Bardijn', 'francois.bardijn@vinci.be', 'françois', 'mdp');
+
+INSERT INTO kwicker.posts (id_user, message)
+VALUES (1, 'Hello World!');
+INSERT INTO kwicker.posts (id_user, message)
+VALUES (1, 'Bye World!');
+INSERT INTO kwicker.posts (id_user, message)
+VALUES (2, 'Je m''appelle Victor et j''aime les présentations orale en anglais');
+INSERT INTO kwicker.posts (id_user, message)
+VALUES (2, 'J''aime ma moto');
+INSERT INTO kwicker.posts (id_user, message)
+VALUES (3, 'Je lis des livres dans le métro et je rate l''arrêt :p');
+INSERT INTO kwicker.posts (id_user, message)
+VALUES (3, 'Ça bosse dure ici.');
+INSERT INTO kwicker.posts (id_user, message)
+VALUES (4, 'JS le sang');
+INSERT INTO kwicker.posts (id_user, message)
+VALUES (4, 'Je m''appelle François');
+
+INSERT INTO kwicker.likes (id_user, id_post)
+VALUES (1, 4);
+INSERT INTO kwicker.likes (id_user, id_post)
+VALUES (2, 8);
+INSERT INTO kwicker.likes (id_user, id_post)
+VALUES (3, 5);
+INSERT INTO kwicker.likes (id_user, id_post)
+VALUES (4, 8);
