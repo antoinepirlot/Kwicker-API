2e84c928ddce7acc2802585a46210414b4d82c47
antoine <pirlotantoine2@gmail.com>
2021-12-15 23:03:00 +0100

    Update to use .env file for db credit

--- db/db.js
+++ db/db.js
@@ -1,6 +1,14 @@
-//From pg domcumentation
+require("dotenv").config();
 const {Pool} = require("pg");
-const dbConfig = require("../config/db_config");
+
+dbConfig = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    ssl: {rejectUnauthorized: false}
+};
 
 const pool = new Pool(dbConfig);
 
