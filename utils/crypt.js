ff19f5926554723f1371627a45baec47c5538b38
antoine <pirlotantoine2@gmail.com>
2021-12-17 16:38:47 +0100

    Update - use base64 at this time

--- utils/crypt.js
+++ utils/crypt.js
@@ -1,34 +1,13 @@
 const cryptoJs = require("crypto-js");
-const bcrypt = require("bcrypt");
-const saltRound = 10;
 
-async function encrypt(message) {
-    const password = await bcrypt.hash(generateP(), saltRound);
-    return {
-        encryptedMessage: cryptoJs.AES.encrypt(message, password).toString(),
-        password: password
-    };
+function encrypt(message) {
+    return cryptoJs.enc.Base64.stringify(cryptoJs.enc.Utf8.parse(message));
 }
 
-function decrypt(encryptedMessage, password) {
-    const bytes = cryptoJs.AES.decrypt(encryptedMessage, password);
-    return bytes.toString(cryptoJs.enc.Utf8);
+function decrypt(encryptedMessage) {
+    return cryptoJs.enc.Base64.parse(encryptedMessage).toString(cryptoJs.enc.Utf8);
 }
 
-//generateP has been copied from: https://www.geeksforgeeks.org/how-to-generate-a-random-password-using-javascript/
-function generateP() {
-    var pass = '';
-    var str = '[ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
-        'abcdefghijklmnopqrstuvwxyz0123456789@#$';
-
-    for (let i = 1; i <= 50; i++) {
-        var char = Math.floor(Math.random()
-            * str.length + 1);
-
-        pass += str.charAt(char)
-    }
-
-    return pass;
-}
+console.log(decrypt(encrypt("Bonjour les amis, je ne sais que faire")));
 
 module.exports = {encrypt, decrypt};
