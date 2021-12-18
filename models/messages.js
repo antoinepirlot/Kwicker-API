ff4fa5bf9b8818071cb7d7200c122dc9561ab01a
antoine <pirlotantoine2@gmail.com>
2021-12-18 14:00:48 +0100

    Deactivate encrypt and decrypt at this time

--- models/messages.js
+++ models/messages.js
@@ -35,7 +35,7 @@ class Messages {
 
         try {
             const {rows} = await db.query(query);
-            decrypt(rows);
+            //decrypt(rows);
             return rows;
         } catch (e) {
             console.log(e.stack);
@@ -54,7 +54,7 @@ class Messages {
         const query = {
             text: `INSERT INTO kwicker.messages (id_sender, id_recipient, message)
                    VALUES ($1, $2, $3)`,
-            values: [escape(id_sender), escape(id_recipient), encrypt(escape(message))]
+            values: [escape(id_sender), escape(id_recipient), escape(message)]
         };
         try {
             const result = await db.query(query);
