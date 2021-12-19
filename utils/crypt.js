1497b51ad2f3de3d69f7adf14ea793ebf5cf7dd4
Antoine <pirlotantoine2@gmail.com>
2021-12-19 16:36:15 +0100

    Update - AES

--- utils/crypt.js
+++ utils/crypt.js
@@ -2,12 +2,12 @@ const cryptoJs = require("crypto-js");
 const messagePassword = process.env.messagesPassword;
 
 function encrypt(message) {
-    return cryptoJs.enc.Base64.stringify(cryptoJs.enc.Utf8.parse(message));
+    return cryptoJs.AES.encrypt(message, messagePassword);
 }
 
 function decrypt(rows) {
     rows.forEach((row) => {
-        row["message"] = cryptoJs.enc.Base64.parse(row["message"]).toString(cryptoJs.enc.Utf8);
+        row["message"] = cryptoJs.AES.decrypt(row["message"], messagePassword).toString(cryptoJs.enc.Utf8);
     });
 }
 
